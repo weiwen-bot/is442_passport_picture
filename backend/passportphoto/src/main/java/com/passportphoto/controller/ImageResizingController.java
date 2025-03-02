b0b9845b9985205df3ec468a3dbafac0faef7f8a
kybae04 <kyliebay@gmail.com>
2025-03-02 19:00:34 +0800

    tweaking bufferedImageToMat method

--- backend/passportphoto/src/main/java/com/passportphoto/controller/ImageResizingController.java
+++ backend/passportphoto/src/main/java/com/passportphoto/controller/ImageResizingController.java
@@ -11,6 +11,7 @@ import java.awt.image.DataBufferByte;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
+import java.awt.Graphics2D;
 
 @RestController
 @RequestMapping("/image")
@@ -64,6 +65,17 @@ public class ImageResizingController {
 
     // Method to convert BufferedImage to OpenCV Mat
     private Mat bufferedImageToMat(BufferedImage image) {
+        int type = image.getType();
+    
+        if (type == BufferedImage.TYPE_BYTE_GRAY) {
+            // Convert grayscale image to RGB (3 channels)
+            BufferedImage rgbImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+            Graphics2D g2d = rgbImage.createGraphics();
+            g2d.drawImage(image, 0, 0, null);
+            g2d.dispose();
+            image = rgbImage;
+        }
+    
         byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
         Mat mat = new Mat(image.getHeight(), image.getWidth(), CvType.CV_8UC3);
         mat.put(0, 0, pixels);
