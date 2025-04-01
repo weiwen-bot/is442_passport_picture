a488828b86530a86d18b67ebec0a1087eb39fcd4
kybae04 <kyliebay@gmail.com>
2025-04-01 18:25:21 +0800

    javadoc and formatting for constants

--- backend/passportphoto/src/main/java/com/passportphoto/util/Constants.java
+++ backend/passportphoto/src/main/java/com/passportphoto/util/Constants.java
@@ -1,7 +1,22 @@
+/*
+ * Constants.java
+ *
+ * This class holds global constants used across the Passport Picture Project.
+ * It includes model file names and default configuration values.
+ *
+ */
+
 package com.passportphoto.util;
 
+/**
+ * The {@code Constants} class contains shared static final values such as
+ * file names and default settings used throughout the application.
+ */
 public class Constants {
 
+    /** The filename of the ONNX model used for background removal */
     public static final String MODEL_NAME = "modnet.onnx";
+
+    /** The default background color to use (in hex) */
     public static final String DEFAULT_BACKGROUND_COLOR = "#FFFFFF";
 }
