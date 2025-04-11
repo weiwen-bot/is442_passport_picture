f20a7c687bb50e9b106dc2a13abb9871299907bb
weiwen <tanweiwen32@gmail.com>
2025-04-11 09:24:46 +0800

    Update FaceCenteringService.java

--- backend/passportphoto/src/main/java/com/passportphoto/service/FaceCenteringService.java
+++ backend/passportphoto/src/main/java/com/passportphoto/service/FaceCenteringService.java
@@ -15,7 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.w3c.dom.css.Rect;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
